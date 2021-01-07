4f5f3f27e4b44aa75e460766f4d91c3c5c35d301
Lourival Linard <l.linard@gmail.com>
2021-01-07 00:09:05 -0300

    Projeto 1 - Aula 4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,6 @@
-module sistema {
+// Responsável por definir as regras de visibilidade do modulo sistema.
+module conta.sistema {
+    // usa spring
+    requires javax.inject;
+    requires spring.tx;
 }
