bfbc74ee2bf23363505c9f6a395e0304a8957f5a
Lourival Linard <l.linard@gmail.com>
2021-01-09 00:19:28 -0300

    Projeto 1 - Aula 20

--- src/main/java/conta/sistema/casouso.imp/PortaTransferenciaImp.java
+++ src/main/java/conta/sistema/casouso.imp/PortaTransferenciaImp.java
@@ -34,7 +34,37 @@ public class PortaTransferenciaImp implements PortaTransferencia {
     }
 
     @Override
+    @Transactional
     public void transferir(Integer contaDebito, Integer contaCredito, BigDecimal valor) {
+        //1. validação de parametros
+        if (isNull(contaDebito)) {
+            obrigatorio("Conta débito");
+        }
+        if (isNull(contaCredito)) {
+            obrigatorio("Conta crédito");
+        }
+        if (isNull(valor)) {
+            obrigatorio("Valor");
+        }
 
+        //2. validação de contas
+        var debito = repositorio.get(contaDebito);
+        if (isNull(debito)) {
+            inexistente("Conta débito");
+        }
+        var credito = repositorio.get(contaCredito);
+        if (isNull(credito)) {
+            inexistente("Conta crédito");
+        }
+
+        //3.validacao mesma conta
+        if (debito.getNumero().equals(credito.getNumero())) {
+            mesmaConta();
+        }
+
+        //4. operação
+        transferencia.processar(valor, debito, credito);
+        repositorio.alterar(debito);
+        repositorio.alterar(credito);
     }
 }
