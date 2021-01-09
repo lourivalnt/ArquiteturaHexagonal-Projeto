ff49292b38a14c632bd6d57fb1627516af93f116
Lourival Linard <l.linard@gmail.com>
2021-01-08 23:53:11 -0300

    Projeto 1 - Aula 16

--- src/main/java/conta/adaptador/AdaptadorContaFakeImp.java
+++ src/main/java/conta/adaptador/AdaptadorContaFakeImp.java
@@ -16,13 +16,22 @@ import static java.util.Objects.isNull;
 @Named
 public class AdaptadorContaFakeImp implements ContaRepositorio {
 
+    private Map<Integer, Conta> banco = new HashMap<>();
+
     @Override
     public Conta get(Integer numero) {
-        return null;
+        System.out.println("Fake banco de dados -> Conta get(numero)");
+        return banco.get(numero);
     }
 
     @Override
     public void alterar(Conta conta) {
-
+        System.out.println("Fake banco de dados -> alterar(conta)");
+        var ct = banco.get(conta.getNumero());
+        if (!isNull(ct)) {
+            banco.put(conta.getNumero(), conta);
+        } else {
+            throw new NegocioException("Conta inexistente: " + conta.getNumero());
+        }
     }
 }
