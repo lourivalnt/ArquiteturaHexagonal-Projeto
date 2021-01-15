03620ebd22ef6a70c2b8de46a215919d6fc5bc63
Lourival Linard <l.linard@gmail.com>
2021-01-14 23:10:05 -0300

    Projeto 1 - Aula 23

--- src/test/java/teste/casouso/TesteAdaptadorTransferencia.java
+++ src/test/java/teste/casouso/TesteAdaptadorTransferencia.java
@@ -1,19 +1,67 @@
 package teste.casouso;
 
+import conta.sistema.casouso.porta.PortaTransferencia;
+import conta.sistema.dominio.modelo.NegocioException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.inject.Inject;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @DisplayName("Caso de Uso - Serviço de Transferência")
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = Build1.class)
 public class TesteAdaptadorTransferencia {
 
+    Integer contaCredito = 10;
+    Integer contaDebito = 20;
+    Integer contaInexistente = 30;
+    BigDecimal cem = new BigDecimal(100);
+    BigDecimal cinquenta = new BigDecimal(50);
+
+    @Inject
+    PortaTransferencia porta;
+
+    // negativos get conta
+
     @Test
     @DisplayName("pesquisa conta com número nulo")
     void teste1() {
-      System.out.println("olá");
+        try {
+            var conta = porta.getConta(null);
+            assertTrue(conta == null, "Conta deve ser nula");
+        } catch (NegocioException e) {
+            fail("Deva carregar uma conta nula.");
+        }
+    }
+
+    @Test
+    @DisplayName("pesquisa conta com número inexistente")
+    void teste2() {
+        try {
+            var conta = porta.getConta(contaInexistente);
+            assertTrue(conta == null, "Conta deve ser nula");
+        } catch (NegocioException e) {
+            fail("Deva carregar uma conta nula.");
+        }
+    }
+
+    // positivo get conta
+
+    @Test
+    @DisplayName("pesquisa conta com número existente")
+    void teste3() {
+        try {
+            var conta = porta.getConta(contaCredito);
+            assertTrue(conta != null, "Conta deve ser nula");
+            System.out.println(conta);
+        } catch (NegocioException e) {
+            fail("Deva carregar uma conta existente.");
+        }
     }
 }
